94ed0034f2fa1b4d531e85d806354f57a06f82b3
Bruno Araujo <bruno.araujo@gmail.com>
2015-03-20 23:35:19 -0400

    Trocando lógica pra detectar se está na plataforma para usar Physics.Linecast

--- Assets/Scripts/Runner.cs
+++ Assets/Scripts/Runner.cs
@@ -7,8 +7,13 @@ public class Runner : MonoBehaviour {
     public float aceleracao;
 	public Vector3 velocidadePulo;
 	public bool temPuloDuplo = false;
-
+    
+    private Transform groundCheck;
     private bool tocouPlataforma;
+
+    void Awake() {
+        groundCheck = transform.FindChild("GroundCheck");
+    }
 
 	void Update () {
 		if(!tocouPlataforma && 
@@ -20,15 +25,12 @@ public class Runner : MonoBehaviour {
 		distanciaPercorrida = transform.localPosition.x;
 
 		if (tocouPlataforma) {
-
 			if(Input.GetButtonDown("Jump") || Input.touchCount > 0){
 				rigidbody.AddForce(velocidadePulo, ForceMode.VelocityChange);
 				temPuloDuplo = true;
 				tocouPlataforma = false;
 			}
-		
 		}
-
 
         if (Input.GetKeyDown(KeyCode.Escape)) {
             Application.Quit();
@@ -36,17 +38,13 @@ public class Runner : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
+        tocouPlataforma = Physics.Linecast(transform.position, 
+                                           groundCheck.position,
+                                           1 << LayerMask.NameToLayer("Ground"));
 		if(tocouPlataforma){
 			rigidbody.AddForce(aceleracao, 0f, 0f, ForceMode.Acceleration);
-			tocouPlataforma=true;
+            Debug.Log("Acelerando");
 		}
-	}
-	
-	void OnCollisionEnter () {
-		tocouPlataforma = true;
-	}
-	
-	void OnCollisionExit () {
-		tocouPlataforma = false;
+        else Debug.Log("não acelerando");
 	}
 }
